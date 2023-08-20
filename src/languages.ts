883ef0736898ef2082ece92ac9ef4518c17e36c9
Lou <lou.yiw@gmail.com>
2023-08-20 12:15:56 +0800

    feat(authpage): add authpage and i18n

---
+++ src/languages.ts
@@ -0,0 +1,33 @@
+/** en, cn */
+/**
+ * use module
+ * module.exports = global.config = {
+    i18n: {
+        welcome: {
+            en: "Welcome",
+            fa: "خوش آمدید"
+        }
+        // rest of your translation object
+    }
+    // other global config variables you wish
+  };
+  *import './config';
+  *global.config.i18n.welcome.en
+ */
+
+const Lang = {
+  authentication: {
+    logIn: { eng: 'Log In', cn: '登入' },
+    signIn: { eng: 'Sign Up', cn: '註冊' },
+    logInSuccess: { eng: 'Successfully logged in', cn: '登入成功' },
+    signInSuccess: { eng: 'Successfully signed up', cn: '註冊成功' },
+    logInFailure: { eng: 'Failed to log in', cn: '登入失敗' },
+    signInFailuire: { eng: 'Failed to sign up', cn: '註冊失敗' },
+  },
+  errorMsg: {
+    minWord: { eng: ['At least ', ' words.'], cn: ['至少', '字'] },
+    required: { eng: 'Required.', cn: '必填' },
+  },
+};
+
+export default Lang;
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,10 +1,49 @@
 import React, { useEffect } from 'react';
+import AuthPage from './AuthPage';
 import { Box } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { setUserInfos } from '../redux/reducers/userSlice';
+import { setSettings } from '../redux/reducers/settingsSlice';
 
 function App(): JSX.Element {
+  const dispatch = useDispatch();
+  const userInfos = useSelector(
+    (state: {
+      userInfos: {
+        userInfos: {
+          user: string;
+          id: string;
+          coupleId: string | null;
+        };
+      };
+    }) => state.userInfos.userInfos,
+  );
+  const settings = useSelector(
+    (state: {
+      settings: {
+        settings: { lang: string; mode: number; isLoading: boolean };
+      };
+    }) => state.settings.settings,
+  );
+
+  //check local storage if the user is logged in
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user');
+
+    if (storedUser) {
+      const user = JSON.parse(storedUser);
+      dispatch(setUserInfos(user));
+    }
+    setTimeout(
+      () => dispatch(setSettings({ ...settings, isLoading: false })),
+      750,
+    );
+  }, []);
 
   return (
     <Box className="App FullPageBox">
+        <AuthPage />
     </Box>
   );
 }
