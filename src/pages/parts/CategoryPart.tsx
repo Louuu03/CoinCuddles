73c14529b299c77997df8fcea10129ad03a2c051
Lou <lou.yiw@gmail.com>
2023-08-30 17:37:35 +0800

    feat(categorypart): add category part display

--- src/pages/parts/CategoryPart.tsx
+++ src/pages/parts/CategoryPart.tsx
@@ -1,8 +1,157 @@
 import React, { useEffect, useState } from 'react';
-import { Box } from '@mui/material';
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Tab,
+  Tabs,
+  ThemeProvider,
+  createTheme,
+} from '@mui/material';
+import {
+  and,
+  collection,
+  getDocs,
+  getFirestore,
+  or,
+  query,
+  where,
+} from 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from '../../firebaseConfig';
+import { useSelector } from 'react-redux';
+import { isEmpty, find, map } from 'lodash';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+interface CategoryData {
+  coupleId: string;
+  name: string;
+  owner: string;
+  currentValue: number;
+  isPrivate: boolean;
+  parentId: string;
+}
 
 function CategoryPart(): JSX.Element {
-  return <Box className="CategoryPart FullPageBox"></Box>;
+  /* 0:Categories | 1: Tags */
+  const [tab, setTab] = useState<number>(0);
+  const [categories, setCategories] = useState<CategoryData[]>();
+
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
+  const userInfos = useSelector(
+    (state: {
+      userInfos: {
+        userInfos: {
+          user: string;
+          id: string;
+          coupleId: string;
+          tutorial: 0 | 1 | 2;
+        };
+      };
+    }) => state.userInfos.userInfos,
+  );
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#ffa500',
+      },
+      secondary: {
+        main: '#E0C2FF',
+        light: '#F5EBFF',
+        contrastText: '#47008F',
+      },
+    },
+  });
+
+  useEffect(() => {
+    const q = query(
+      collection(db, 'categories'),
+      and(
+        where('coupleId', '==', userInfos.coupleId),
+        or(
+          where('isPrivate', '==', false),
+          and(
+            where('isPrivate', '==', true),
+            where('owner', '==', userInfos.id),
+          ),
+        ),
+      ),
+    );
+    userInfos.coupleId &&
+      getDocs(q).then((res) => {
+        const docs: CategoryData[] = [];
+        res.forEach((doc) => {
+          // Handle each retrieved document here
+          const d = doc.data() as CategoryData;
+          docs.push(d);
+        });
+        setCategories(docs);
+      });
+  }, []);
+
+  return (
+    <Box className="CategoryPart FullPageBox">
+      <ThemeProvider theme={theme}>
+        <Tabs
+          className="Tab"
+          value={tab}
+          onChange={(e, v) => setTab(v)}
+          variant="fullWidth"
+          indicatorColor="primary"
+        >
+          <Tab label="Categories" />
+          <Tab label="Tags" />
+        </Tabs>
+      </ThemeProvider>
+      {tab === 0 ? (
+        <Box className="Category">
+          {map(categories, (category, cIdx) => {
+            return (
+              category.parentId === 0 && (
+                <Box className={`Accordion ${'A' + cIdx}`} key={'A' + cIdx}>
+                  <Accordion className="AccordionBox">
+                    <AccordionSummary
+                      expandIcon={<ExpandMoreIcon />}
+                      className="AccordionTitle"
+                    >
+                      <Box className="TitleBox">
+                        <Box>{category.name}</Box>
+                        <Box className="TitleTotal">
+                          {category.currentValue}
+                        </Box>
+                      </Box>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                      {map(categories, (subCategory, idx) => {
+                        return (
+                          subCategory.parentId === category.id && (
+                            <Box className="Detail" key={idx}>
+                              <Box className="DName">{subCategory.name}</Box>
+                              <Box>{subCategory.currentValue}</Box>
+                            </Box>
+                          )
+                        );
+                      })}
+                      <Box className="Detail">
+                        <Box>Total</Box>
+                        <Box>{category.currentValue}</Box>
+                      </Box>
+                    </AccordionDetails>
+                  </Accordion>
+                </Box>
+              )
+            );
+          })}
+        </Box>
+      ) : (
+        <Box className="Tag">Tag</Box>
+      )}
+    </Box>
+  );
 }
 
 export default CategoryPart;
